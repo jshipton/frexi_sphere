3dc094358d860ccd76efd70656e9e20f20bd6ae5
Jemma Shipton <j.shipton@imperial.ac.uk>
2017-01-09 11:07:46 +0000

    get correct coefficients and loop over

--- test_rexi.py
+++ test_rexi.py
@@ -1,4 +1,5 @@
 from firedrake import *
+from rexi_coefficient_python import REXI
 
 Mesh = IcosahedralSphereMesh(radius=1.0, refinement_level=3, degree=1)
 
@@ -9,13 +10,18 @@ V1 = FunctionSpace(Mesh,"BDM",1)
 V2 = FunctionSpace(Mesh,"DG",0)
 
 f = Constant(1.0)
-g = Constant(1.0)
+g = Constant(9.8)
 H = Constant(1.0)
+
+h = 0.2
+M = 64
+rexi = REXI(h, M)
 
 ai = Constant(0.0)
 bi = Constant(0.0)
 ar = Constant(1.0)
 br = Constant(1.0)
+dt = Constant(1.)
 
 W = MixedFunctionSpace((V1,V2,V1,V2))
 
@@ -29,12 +35,12 @@ outward_normals = CellNormal(Mesh)
 perp = lambda u: cross(outward_normals, u)
 
 a = (
-    inner(wr,u1r)*ar + f*inner(wr,perp(u1r)) - g*div(wr)*h1r 
+    inner(wr,u1r)*ar - dt*f*inner(wr,perp(u1r)) + dt*g*div(wr)*h1r 
     - ai*inner(wr,u1i)
-    + phr*(ai*h1r + H*div(u1r) - ar*h1i)
-    + inner(wi,u1i)*ar + f*inner(wi,perp(u1i)) - g*div(wi)*h1i 
+    + phr*(ai*h1r - dt*H*div(u1r) - ar*h1i)
+    + inner(wi,u1i)*ar - dt*f*inner(wi,perp(u1i)) + dt*g*div(wi)*h1i 
     + ai*inner(wi,u1r)
-    + phi*(ai*h1i + H*div(u1i) + ar*h1r)
+    + phi*(ai*h1i - dt*H*div(u1i) + ar*h1r)
 )*dx
 
 L = (
@@ -55,9 +61,18 @@ lu_solver_parameters = {'ksp_type':'preonly',
 rexi_solver = LinearVariationalSolver(myprob,
                                       solver_parameters=lu_solver_parameters)
 
-rexi_solver.solve()
+w_sum = Function(W)
 
-u1r_,h1r_,u1i_,h1i_ = w.split()
+for i in range(len(rexi.alpha)):
+    ai.assign(rexi.alpha[i].imag)
+    ar.assign(rexi.alpha[i].real)
+    bi.assign(rexi.beta_re[i].imag)
+    br.assign(rexi.beta_re[i].real)
+
+    rexi_solver.solve()
+    w_sum += w
+
+u1r_,h1r_,u1i_,h1i_ = w_sum.split()
 
 u1r = Function(V1,name="u1r").assign(u1r_)
 u1i = Function(V1,name="u1i").assign(u1i_)
