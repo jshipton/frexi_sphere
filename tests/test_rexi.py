10f943e14d76dec4c03ab0e1ebdbd39098268f73
Jemma Shipton <j.shipton@imperial.ac.uk>
2017-01-31 21:45:35 +0000

    more tidying

--- tests/test_rexi.py
+++ tests/test_rexi.py
@@ -11,34 +11,25 @@ mu = params.mu
 L = params.L
 a = params.a
 
-def approx_e_ix(x):
-    sum_re = 0;
-    sum_im = 0;
+def approx_e_ix(x, use_Gaussian_approx):
 
-    S = len(alpha)
-
-    # Split computation into real part of \f$ cos(x) \f$ and imaginary part \f$ sin(x) \f$
-    for n in range(0, S):
-	denom = (1j*x + alpha[n]);
-	sum_re += (beta_re[n] / denom).real
-	sum_im += (beta_im[n] / denom).real
-
-    return sum_re + 1j*sum_im;
-
-def approx_e_ix2(x):
     sum = 0
-    for m in range(-M, M+1):
-	sum += b[m+M] * approxGaussian(x+float(m)*h, h)
+    if use_Gaussian_approx:
+        for m in range(-M, M+1):
+	    sum += b[m+M] * approxGaussian(x+float(m)*h, h)
+    else:
+        for n in range(len(alpha)):
+	    denom = (1j*x + alpha[n]);
+	    sum += (beta_re[n] / denom).real + 1j*(beta_im[n] / denom).real
 
     return sum
+
 
 def approxGaussian(x, h):
     """
     evaluate approximation of Gaussian basis function
     with sum of complex rational functions
     """
-    # scale x, since it depends linearly on h:
-    # x^2 ~ h^2
     x /= h
 
     sum = 0
@@ -56,21 +47,20 @@ def test_exponential_approx():
 
     for x in range(-int(h*M)+1, int(h*M)):
         exact = exp(1j*x)
-        approx = approx_e_ix2(x)
-        assert abs(exact - approx) < 5.e-8
+        approx = approx_e_ix(x, True)
+        assert abs(exact - approx) < 2.e-11
 
 
 def test_rexi_gaussian_approx():
-    h = 1
     for x in range(10):
         exact = exp(-(x*x)/(4.0*h*h))/sqrt(4.0*pi)
         approx = approxGaussian(x, h)
-        assert abs(exact - approx) < 1.e-12
+        assert abs(exact - approx) < 7.15344e-13
 
 
-def test_rexi():
+def test_rexi_exponential_approx():
 
     for x in range(-int(h*M)+1, int(h*M)):
         exact = exp(1j*x)
-        approx = approx_e_ix(x)
-        assert abs(exact - approx) < 1.5
+        approx = approx_e_ix(x, False)
+        assert abs(exact - approx) < 2.e-11
