6c31db5414fa018257f145223b60cb6b4ad49fcb
Jemma Shipton <j.shipton@imperial.ac.uk>
2017-03-08 13:00:42 +0000

    Add some diagnostics and use them in the Williamson 2 test.

---
+++ frexi_sphere/diagnostics.py
@@ -0,0 +1,44 @@
+from firedrake import assemble, inner, dx, sqrt, op2, dot, FunctionSpace, Function
+import numpy as np
+
+class Diagnostics(object):
+
+    @staticmethod
+    def min(f_in):
+        fmin = op2.Global(1, [np.finfo(float).max], dtype=float)
+        if len(f_in.ufl_shape) > 0:
+            mesh = f_in.function_space().mesh()
+            V = FunctionSpace(mesh, "DG", 1)
+            f = Function(V).project(sqrt(inner(f_in, f_in)))
+        else:
+            f = f_in
+        op2.par_loop(op2.Kernel("""void minify(double *a, double *b)
+        {
+        a[0] = a[0] > fabs(b[0]) ? fabs(b[0]) : a[0];
+        }""", "minify"),
+                     f.dof_dset.set, fmin(op2.MIN), f.dat(op2.READ))
+        return fmin.data[0]
+
+    @staticmethod
+    def max(f_in):
+        fmax = op2.Global(1, [np.finfo(float).min], dtype=float)
+        if len(f_in.ufl_shape) > 0:
+            mesh = f_in.function_space().mesh()
+            V = FunctionSpace(mesh, "DG", 1)
+            f = Function(V).project(sqrt(inner(f_in, f_in)))
+        else:
+            f = f_in
+        op2.par_loop(op2.Kernel("""void maxify(double *a, double *b)
+        {
+        a[0] = a[0] < fabs(b[0]) ? fabs(b[0]) : a[0];
+        }""", "maxify"),
+                     f.dof_dset.set, fmax(op2.MAX), f.dat(op2.READ))
+        return fmax.data[0]
+
+    @staticmethod
+    def l2(f):
+        return sqrt(assemble(dot(f, f)*dx))
+
+    @staticmethod
+    def energy(h, u, g):
+        return assemble(0.5*h*(inner(u, u) + g*h)*dx)
--- frexi_sphere/williamson2.py
+++ frexi_sphere/williamson2.py
@@ -1,14 +1,17 @@
 from firedrake import *
 from frexi_sphere.exponential_integrators import ETD1, ETD2RK
 from frexi_sphere.sw_setup import SetupShallowWater
+import frexi_sphere.diagnostics
 from os import path
+import json
 
 # set up mesh and initial conditions for Williamson 2 testcase
 R = 6371220.
 mesh = IcosahedralSphereMesh(radius=R, refinement_level=3, degree=3)
 global_normal = Expression(("x[0]", "x[1]", "x[2]"))
 mesh.init_cell_orientations(global_normal)
-setup = SetupShallowWater(mesh, family="BDM", degree=1, problem_name="w2")
+degree = 1
+setup = SetupShallowWater(mesh, family="BDM", degree=degree, problem_name="w2")
 
 # get spaces and initialise
 V1 = setup.spaces['u']
@@ -24,27 +27,70 @@ h_init = Function(V2,name="h_init").assign(setup.h0)
 u_err = Function(V1,name="u_err")
 h_err = Function(V2,name="h_err")
 
+# setup parameters for timestepping and REXI
 t = 0.
-tmax = 5*24.*60.*60.
 dt = 3000.
-
+tmax = 5*24.*60.*60.
 h = 0.2
 M = 64
+
+# make timestepper
 timestepper = ETD2RK(setup, dt, True, h, M, False)
 
-filename = path.join('results', 'rexi_w2_dt'+str(dt)+'_h'+str(h)+'_M'+str(M)+'.pvd')
+# output file and output fields
+dirname = 'w2_deg%s_dt%s_h%s_M%s' % (degree, dt, h, M)
+filename = path.join('results/'+dirname, 'output.pvd')
 outfile = File(filename)
-outfile.write(u0, h0, u_err, h_err)
+fields = [u0, h0, u_err, h_err]
+outfile.write(*fields)
 
-while t - 0.5*dt < tmax:
+# setup diagnostics
+field_dict = {field.name(): field for field in fields}
+diagnostics = frexi_sphere.diagnostics.Diagnostics()
+diagnostics_list = ['max', 'min', 'l2']
+diagnostics_dict = {name: getattr(diagnostics, name) for name in diagnostics_list}
+diagnostics_data = {}
+for name, field in field_dict.iteritems():
+    diagnostics_data[name] = {}
+    for diagnostic in diagnostics_list:
+        diagnostics_data[name][diagnostic] = []
+
+diagnostics_data['time'] = []
+diagnostics_data['energy'] = []
+
+# write out initial diagnostics
+for fname, field in field_dict.iteritems():
+    for dname, diagnostic in diagnostics_dict.iteritems():
+        diagnostics_data[fname][dname].append(diagnostic(field))
+
+diagnostics_data['energy'].append(diagnostics.energy(h0, u0, setup.params.g))
+diagnostics_data['time'].append(t)
+
+# print time and energy to check things are going well    
+print t, diagnostics_data['energy'][-1]
+
+# timestepping loop
+while t < tmax - 0.5*dt:
     
-    t += dt
     timestepper.apply(u0, h0, u_out, h_out)
     u0.assign(u_out)
     h0.assign(h_out)
     u_err.assign(u_out - u_init)
     h_err.assign(h_out - h_init)
 
-    outfile.write(u0, h0, u_err, h_err)
+    outfile.write(*fields)
+    t += dt
 
+    for fname, field in field_dict.iteritems():
+        for dname, diagnostic in diagnostics_dict.iteritems():
+            diagnostics_data[fname][dname].append(diagnostic(field))
+    diagnostics_data['energy'].append(diagnostics.energy(h0, u0, setup.params.g))
+    diagnostics_data['time'].append(t)
+
+    # print time and energy to check things are going well    
+    print t, diagnostics_data['energy'][-1]
+
+# dump diagnostics dictionary
+with open(path.join("results/"+dirname, "diagnostics.json"), "w") as f:
+    f.write(json.dumps(diagnostics_data, indent=4))
 
