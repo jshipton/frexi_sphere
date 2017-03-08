5d37d509bc6a3d08c6ecc2adb01639cf1e4ae486
Jemma Shipton <j.shipton@imperial.ac.uk>
2017-03-08 15:46:44 +0000

    Separate timestepping loop from problem setup to make it easier to run other tests.

---
+++ frexi_sphere/timestepping.py
@@ -0,0 +1,95 @@
+from firedrake import *
+import frexi_sphere.diagnostics
+from os import path
+import json
+
+class Timestepping(object):
+
+    def __init__(self, dirname, fields, params, timestepper):
+        filename = path.join('results/'+dirname, 'output.pvd')
+        self.outfile = File(filename)
+        self.fields = fields
+        self.field_dict = {field.name(): field for field in fields}
+        self.params = params
+        self.timestepper = timestepper
+        self.diagnostics_file = path.join("results/"+dirname, "diagnostics.json")
+        self.diagnostics = frexi_sphere.diagnostics.Diagnostics()
+
+    def setup_diagnostics(self):
+
+        diagnostics_list = ['max', 'min', 'l2']
+        self.diagnostics_dict = {name: getattr(self.diagnostics, name) for name in diagnostics_list}
+        self.diagnostics_data = {}
+        for name, field in self.field_dict.iteritems():
+            self.diagnostics_data[name] = {}
+            for diagnostic in diagnostics_list:
+                self.diagnostics_data[name][diagnostic] = []
+
+        self.diagnostics_data['time'] = []
+        self.diagnostics_data['energy'] = []
+
+    def run(self, dt, tmax, steady_state=False):
+
+        # get initial fields
+        u0 = self.field_dict['u']
+        h0 = self.field_dict['h']
+
+        # save initial conditions for computing errors and set up error fields
+        V1 = u0.function_space()
+        V2 = h0.function_space()
+        if steady_state:
+            u_init = Function(V1,name="u_init").assign(u0)
+            u_err = Function(V1,name="u_err")
+            self.field_dict['u_err'] = u_err
+            h_init = Function(V2,name="h_init").assign(h0)
+            h_err = Function(V2,name="h_err")
+            self.field_dict['h_err'] = h_err
+
+        # setup diagnostics
+        self.setup_diagnostics()
+
+        # make functions for timestepping
+        u_out = Function(V1)
+        h_out = Function(V2)
+        t = 0.
+
+        # dump initial fields
+        self.outfile.write(*self.fields)
+
+        # save initial diagnostics
+        for fname, field in self.field_dict.iteritems():
+            for dname, diagnostic in self.diagnostics_dict.iteritems():
+                self.diagnostics_data[fname][dname].append(diagnostic(field))
+
+        self.diagnostics_data['energy'].append(self.diagnostics.energy(h0, u0, self.params.g))
+        self.diagnostics_data['time'].append(t)
+
+        # print time and energy to check things are going well    
+        print t, self.diagnostics_data['energy'][-1]
+
+        # timestepping loop
+        while t < tmax - 0.5*dt:
+    
+            self.timestepper.apply(u0, h0, u_out, h_out)
+            u0.assign(u_out)
+            h0.assign(h_out)
+            if steady_state:
+                u_err.assign(u_out - u_init)
+                h_err.assign(h_out - h_init)
+
+            self.outfile.write(*self.fields)
+            t += dt
+
+            for fname, field in self.field_dict.iteritems():
+                for dname, diagnostic in self.diagnostics_dict.iteritems():
+                    self.diagnostics_data[fname][dname].append(diagnostic(field))
+            self.diagnostics_data['energy'].append(self.diagnostics.energy(h0, u0, self.params.g))
+            self.diagnostics_data['time'].append(t)
+
+            # print time and energy to check things are going well    
+            print t, self.diagnostics_data['energy'][-1]
+
+        # dump diagnostics dictionary
+        with open(self.diagnostics_file, "w") as f:
+            f.write(json.dumps(self.diagnostics_data, indent=4))
+
--- frexi_sphere/williamson2.py
+++ frexi_sphere/williamson2.py
@@ -2,6 +2,7 @@ from firedrake import *
 from frexi_sphere.exponential_integrators import ETD1, ETD2RK
 from frexi_sphere.sw_setup import SetupShallowWater
 import frexi_sphere.diagnostics
+import frexi_sphere.timestepping
 from os import path
 import json
 
@@ -18,17 +19,8 @@ V1 = setup.spaces['u']
 V2 = setup.spaces['h']
 u0 = Function(V1,name="u").assign(setup.u0)
 h0 = Function(V2,name="h").assign(setup.h0)
-u_out = Function(V1)
-h_out = Function(V2)
-
-# save initial conditions for computing errors and set up error fields
-u_init = Function(V1,name="u_init").assign(setup.u0)
-h_init = Function(V2,name="h_init").assign(setup.h0)
-u_err = Function(V1,name="u_err")
-h_err = Function(V2,name="h_err")
 
 # setup parameters for timestepping and REXI
-t = 0.
 dt = 3000.
 tmax = 5*24.*60.*60.
 h = 0.2
@@ -39,58 +31,7 @@ timestepper = ETD2RK(setup, dt, True, h, M, False)
 
 # output file and output fields
 dirname = 'w2_deg%s_dt%s_h%s_M%s' % (degree, dt, h, M)
-filename = path.join('results/'+dirname, 'output.pvd')
-outfile = File(filename)
-fields = [u0, h0, u_err, h_err]
-outfile.write(*fields)
+fields = [u0, h0]
 
-# setup diagnostics
-field_dict = {field.name(): field for field in fields}
-diagnostics = frexi_sphere.diagnostics.Diagnostics()
-diagnostics_list = ['max', 'min', 'l2']
-diagnostics_dict = {name: getattr(diagnostics, name) for name in diagnostics_list}
-diagnostics_data = {}
-for name, field in field_dict.iteritems():
-    diagnostics_data[name] = {}
-    for diagnostic in diagnostics_list:
-        diagnostics_data[name][diagnostic] = []
-
-diagnostics_data['time'] = []
-diagnostics_data['energy'] = []
-
-# write out initial diagnostics
-for fname, field in field_dict.iteritems():
-    for dname, diagnostic in diagnostics_dict.iteritems():
-        diagnostics_data[fname][dname].append(diagnostic(field))
-
-diagnostics_data['energy'].append(diagnostics.energy(h0, u0, setup.params.g))
-diagnostics_data['time'].append(t)
-
-# print time and energy to check things are going well    
-print t, diagnostics_data['energy'][-1]
-
-# timestepping loop
-while t < tmax - 0.5*dt:
-    
-    timestepper.apply(u0, h0, u_out, h_out)
-    u0.assign(u_out)
-    h0.assign(h_out)
-    u_err.assign(u_out - u_init)
-    h_err.assign(h_out - h_init)
-
-    outfile.write(*fields)
-    t += dt
-
-    for fname, field in field_dict.iteritems():
-        for dname, diagnostic in diagnostics_dict.iteritems():
-            diagnostics_data[fname][dname].append(diagnostic(field))
-    diagnostics_data['energy'].append(diagnostics.energy(h0, u0, setup.params.g))
-    diagnostics_data['time'].append(t)
-
-    # print time and energy to check things are going well    
-    print t, diagnostics_data['energy'][-1]
-
-# dump diagnostics dictionary
-with open(path.join("results/"+dirname, "diagnostics.json"), "w") as f:
-    f.write(json.dumps(diagnostics_data, indent=4))
-
+timestepping = frexi_sphere.timestepping.Timestepping(dirname, fields, setup.params, timestepper)
+timestepping.run(dt, tmax, steady_state=True)
